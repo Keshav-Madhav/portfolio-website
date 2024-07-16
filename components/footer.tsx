cc1ebfc860b8269f367e399cec77841d4b19d67a
Keshav Madhav <keshav2552003@gmail.com>
2024-07-16 18:03:58 +0530

    Updated email and added projects

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,10 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="mb-10 px-4 text-center text-gray-500">
-      <small className="mb-2 block text-xs">
-        &copy; 2030 Ricardo. All rights reserved.
-      </small>
+    <footer className="mb-4 px-4 text-center text-gray-500">
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
         React & Next.js (App Router & Server Actions), TypeScript, Tailwind CSS,
--- lib/data.ts
+++ lib/data.ts
@@ -1,6 +1,8 @@
 import clonepen from "@/public/clonepen.png";
 import notesapp from "@/public/notesapp.png";
 import tetris from "@/public/Tetris.png";
+import yoom from "@/public/yoom.png";
+import infinitecraft from "@/public/infinitecraft.png";
 
 export const links = [
   {
@@ -38,15 +40,31 @@ export const projectsData = [
     title: "A notes app",
     description:
       "A notes app where you can create, edit and delete notes. You can also add voice notes and video notes.",
-    tags: ["React", "TypeScript", "Tailwind", "Zustand", "Framer Motion"],
+    tags: ["React", "TypeScript", "Tailwind", "Zustand", "Framer"],
     imageUrl: notesapp,
     redirectURL: "https://a-notes-app.netlify.app/",
+  },
+  {
+    title: "Yoom - Video Meetings",
+    description:
+      "A video calling web application with functionality to create instant meetings, join meetings and schedule meetings.",
+    tags: ["React", "Next.js", "stream.io", "Tailwind", "Clerk"],
+    imageUrl: yoom,
+    redirectURL: "https://zoom-clone-black-sigma.vercel.app/",
+  },
+  {
+    title: "Infinite Craft",
+    description:
+      "A merge and craft game made using GBT-4o to have infinite possibilities. Create any item by combining any two items.",
+    tags: ["Next.js", "Open-AI", "Tailwind", "Framer", "LangChain"],
+    imageUrl: infinitecraft,
+    redirectURL: "https://tetris-keshav-madhav.netlify.app/",
   },
   {
     title: "Tetris Game",
     description:
       "A Tetris game made in react with the help of react hooks. It has features like start, pause, resume and restart.",
-    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    tags: ["React", "Next.js", "Tailwind", "Framer"],
     imageUrl: tetris,
     redirectURL: "https://tetris-keshav-madhav.netlify.app/",
   },
